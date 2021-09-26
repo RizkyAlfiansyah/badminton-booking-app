e348ddcf2ffdb4de1cdbab568e3d9d54c574377d
RizkyAlfiansyah <rizkkyaf@gmail.com>
2021-09-27 00:05:46 +0800

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('temp/head')
+<body>
+    <div id="app">
+        @include('temp/sidebar')
+        <div id="main">
+            @yield('content')
+
+            <footer>
+                <div class="footer clearfix mb-0 text-muted">
+                    <div class="float-start">
+                        <p>2021 &copy; Mazer</p>
+                    </div>
+                    <div class="float-end">
+                        <p>Crafted with <span class="text-danger"><i class="bi bi-heart"></i></span> by <a
+                                href="http://ahmadsaugi.com">A. Saugi</a></p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+  @include('temp/js')
+</body>
+
+</html>
